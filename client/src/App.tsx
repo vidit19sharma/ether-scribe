a318b98e6ff5963e02efd3f602f91b286de873c9
Vidit Sharma <vidit19sharma@gmail.com>
2026-01-14 19:44:56 +0530

    feat(client): setup React + TypeScript frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+// import statements
+import './App.css'
+
+//Main App
+function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Collaborative Editor</h1>
+      <p>Day 1 setup complete 🚀</p>
+    </div>
+  )
+}
+
+export default App
